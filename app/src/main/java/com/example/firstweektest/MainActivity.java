354a8a847c3f2a4945f8a82fe09b6cb6a7b2c89c
yijiannijuxiao <1782946883@qq.com>
2017-09-12 10:02:41 +0800

    第二次提交

--- app/src/main/java/com/example/firstweektest/MainActivity.java
+++ app/src/main/java/com/example/firstweektest/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.firstweektest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("123","1234567890");
     }
 }
